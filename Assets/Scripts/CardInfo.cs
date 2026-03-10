f2844f57c70929fef9866ca79a3a9a5d9b208f4b
TheBlipBloop <blipbloopdev@gmail.com>
2026-03-09 17:31:51 -0700

    flashy

--- Assets/Scripts/CardInfo.cs
+++ Assets/Scripts/CardInfo.cs
@@ -12,8 +12,14 @@ public class CardInfo : ScriptableObject
     [TextArea] public string Description = "really awesome playing card";
     [SerializeField] public List<CardEffect> Effects = new List<CardEffect>();
 
+    public Color Flash = new Color(1, 1, 1, 0.15f);
+
+    public float FlashDecaySpeed = 10;
+
     public void Apply(GameState gameState)
     {
+        FlashOverlay.Flash(Flash, FlashDecaySpeed);
+
         foreach (var effect in Effects)
         {
             effect.Apply(gameState);
