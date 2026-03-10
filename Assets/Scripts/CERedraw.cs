439d92b1adaca7040a26c9788e9311930dee7b8d
Lilith899 <crimmer2@depaul.edu>
2026-03-09 17:08:02 -0700

    getting them cards in boi

--- Assets/Scripts/CERedraw.cs
+++ Assets/Scripts/CERedraw.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CERedraw", menuName = "Scriptable Objects/CERedraw")]
 public class CERedraw : CardEffect
 {
+    public int GuestsDelta;
+    public int MoodDelta;
+    public int SacrificesDelta;
+
     public override void Apply(GameState gameState)
     {
         Game.ReplaceHand();
+        gameState.ChangeGuests(GuestsDelta, false);
+        gameState.ChangeMood(MoodDelta);
+        gameState.ChangeSacrifices(SacrificesDelta);
     }
 }
