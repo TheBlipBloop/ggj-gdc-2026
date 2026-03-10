d8b664f4bfa074f59b175db2093b1298c007095c
TheBlipBloop <blipbloopdev@gmail.com>
2026-03-10 10:01:06 -0700

    peace.

--- Assets/Scripts/Graveyard.cs
+++ Assets/Scripts/Graveyard.cs
@@ -21,68 +21,69 @@ public class Graveyard : MonoBehaviour
     [SerializeField]
     protected Vector2 graveSpacing = new Vector2(0.25f, 0.5f);
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Events.OnSacrificesChanged.AddListener(UpdateGraveCount);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    protected void UpdateGraveCount(int delta)
-    {
-        Debug.Assert(delta != 0);
-
-        bool add = delta > 0;
-        if (add)
+    /*
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            for (int i = 0; i < delta; i++)
+            Events.OnSacrificesChanged.AddListener(UpdateGraveCount);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        protected void UpdateGraveCount(int delta)
+        {
+            Debug.Assert(delta != 0);
+
+            bool add = delta > 0;
+            if (add)
             {
-                AddGrave();
+                for (int i = 0; i < delta; i++)
+                {
+                    AddGrave();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < -delta; i++)
+                {
+                    RemoveGrave();
+                }
             }
         }
-        else
+
+        protected void AddGrave()
         {
-            for (int i = 0; i < -delta; i++)
-            {
-                RemoveGrave();
-            }
+            GameObject newGrave = Instantiate(gravePrefab);
+            _graves.Add(newGrave);
+
+            newGrave.transform.position = GetGravePosition(_graves.Count);
+
+            print("adding grave");
         }
-    }
 
-    protected void AddGrave()
-    {
-        GameObject newGrave = Instantiate(gravePrefab);
-        _graves.Add(newGrave);
+        protected void RemoveGrave()
+        {
+            _graves.RemoveAt(_graves.Count - 1);
+            Destroy(_graves[_graves.Count - 1]);
 
-        newGrave.transform.position = GetGravePosition(_graves.Count);
+            print("remove grave");
+        }
 
-        print("adding grave");
-    }
+        protected Vector3 GetGravePosition(int graveIndex)
+        {
+            // TODO : Fix this when the mind may math.
+            // its wrong.
+            int row = graveIndex / graveColumns;
+            int col = graveIndex % graveColumns;
 
-    protected void RemoveGrave()
-    {
-        _graves.RemoveAt(_graves.Count - 1);
-        Destroy(_graves[_graves.Count - 1]);
+            float x = row * graveSpacing.x;
+            float y = col * graveSpacing.y;
 
-        print("remove grave");
-    }
-
-    protected Vector3 GetGravePosition(int graveIndex)
-    {
-        // TODO : Fix this when the mind may math.
-        // its wrong.
-        int row = graveIndex / graveColumns;
-        int col = graveIndex % graveColumns;
-
-        float x = row * graveSpacing.x;
-        float y = col * graveSpacing.y;
-
-        return new Vector3(x, y, 0) + graveOrigin.position;
-    }
+            return new Vector3(x, y, 0) + graveOrigin.position;
+        }
+        */
 }
